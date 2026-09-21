9c0a82ea827f08f7985315f1f4ca4ecb179f38d0
Cid-Emmerich <324686386+Cid-Emmerich@users.noreply.github.com>
2026-09-21 01:05:47 -0800

    Update wvfrm to 1.1.5
    Also use whisper.cpp, the renamed Homebrew name of the whisper-cpp dependency.

--- Formula/wvfrm.rb
+++ Formula/wvfrm.rb
@@ -1,14 +1,14 @@
 class Wvfrm < Formula
   desc "Terminal music player with visualizers, album art, lyrics and themes"
   homepage "https://github.com/Cid-Emmerich/wvfrm"
-  url "https://github.com/Cid-Emmerich/wvfrm/archive/refs/tags/v1.1.4.tar.gz"
-  sha256 "6cc2aafbb2b7e6b8d970d601da02fe17fba0605dc9dd028ab19eb0bf5b3a1502"
+  url "https://github.com/Cid-Emmerich/wvfrm/archive/refs/tags/v1.1.5.tar.gz"
+  sha256 "d9818c648ccf8470b5e76f8c469b6a0903c7a640b082500ab1413b98e6f4d7c2"
   license "MIT"
   head "https://github.com/Cid-Emmerich/wvfrm.git", branch: "main"
 
   depends_on "go" => :build
   depends_on "ffmpeg"
-  depends_on "whisper-cpp"
+  depends_on "whisper.cpp"
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"wvfrm"), "./cmd/wvfrm"
